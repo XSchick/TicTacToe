79674f98d2c6e26bf65a1eb10ef3d3934b5beb13
Xaver Schick <Xaver.schick@web.de>
2020-12-09 21:27:29 +0100

    Implementing method inputGameplay() to convert argument to int array, which contains the play moves

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 import edu.kit.informatik.Terminal;
 
 /**
- * @xschick
  * @version 1.1
+ * @xschick
  */
 public class Main {
-    int[] gameBoard;
-    int[] gameplay;
-    public static void main(String[] args){
+    int[] gameBoard = new int[9];
+    int[] gameplay = new int[9];
+
+    public static void main(String[] args) {
         new Main(args);
     }
-    public Main(String[] args){
-        inputGameplay(args[0]);
+
+    public Main(String[] args) {
+        inputGameplay(args);
+
     }
 
-    public int[] inputGameplay(String theGameplay){
-        
+    public int[] inputGameplay(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            this.gameplay[i] = Integer.parseInt(args[i]);
+        }
+        return gameplay;
+
+    }
+
+    public void playMove() {
+
     }
 }
