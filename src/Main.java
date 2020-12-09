d9f5ff22d4046b17a5cca7c3c5e60c1efec58832
Xaver Schick <Xaver.schick@web.de>
2020-12-09 21:05:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import edu.kit.informatik.Terminal;
+
+/**
+ * @xschick
+ * @version 1.1
+ */
+public class Main {
+    int[] gameBoard;
+    int[] gameplay;
+    public static void main(String[] args){
+        new Main(args);
+    }
+    public Main(String[] args){
+        inputGameplay(args[0]);
+    }
+
+    public int[] inputGameplay(String theGameplay){
+        
+    }
+}
