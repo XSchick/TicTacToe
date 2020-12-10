177240681500e4a0b904396fea24a40f09a4e40f
Xaver Schick <Xaver.schick@web.de>
2020-12-10 02:12:50 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import edu.kit.informatik.Terminal;
-
 /**
  * @author xschick
  * @version 1.1
@@ -20,20 +18,20 @@ public class Main {
         inputGameplay(args);
         for (int i = 0; i < this.gameplay.length; i++) {
             latestPlayer = playMove(i);
-            System.out.println(i);
             if (checkWin()) {
                 winner = latestPlayer;
-                playMoveNumber = i + 1;
-
+                if (playMoveNumber == 0) {
+                    playMoveNumber = i + 1;
+                }
             }
         }
         System.out.println(winner + " wins " + playMoveNumber);
     }
 
     private boolean checkWin() {
-        System.out.println("colums: " + checkColumnsForWin());
+        /*System.out.println("colums: " + checkColumnsForWin());
         System.out.println("rows: " + checkRowsForWin());
-        System.out.println("diagonal: " + checkDiagonalForWin());
+        System.out.println("diagonal: " + checkDiagonalForWin());*/
         return (checkColumnsForWin() || checkDiagonalForWin() || checkRowsForWin());
     }
 
@@ -66,11 +64,10 @@ public class Main {
         return (((f1 != "-") || (f2 != "-") || (f3 != "-")) && (f1 == f2) && (f2 == f3));
     }
 
-    private int[] inputGameplay(String[] args) {
+    private void inputGameplay(String[] args) {
         for (int i = 0; i < args.length; i++) {
             this.gameplay[i] = Integer.parseInt(args[i]);
         }
-        return gameplay;
 
     }
 
