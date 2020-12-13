b64a117983e4355fdfcd47ae34ea11b26b5b7874
Xaver Schick <Xaver.schick@web.de>
2020-12-13 12:28:07 +0100

    Adding comments and JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import edu.kit.informatik.Terminal;
+
 /**
  * @author xschick
  * @version 1.2
@@ -6,29 +8,46 @@ public final class Main {
     private String[] gameBoard = {"-", "-", "-", "-", "-", "-", "-", "-", "-"};
     private int[] gameplay = new int[9];
 
+    /**
+     * Constructor
+     *
+     * @param args the gameplay
+     */
     private Main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             this.gameplay[i] = Integer.parseInt(args[i]);
         }
     }
 
+    /**
+     * This method calls the playMove() method for every play move
+     * and calls checkWin() to check the field for a win
+     */
     public void game() {
-        String latestPlayer;
+        String actualPlayer;
         String winner = null;
         int moveNumber = 0;
         for (int i = 0; i < this.gameplay.length; i++) {
-            latestPlayer = playMove(i);
+            actualPlayer = playMove(i);
             if (checkWin()) {
-                winner = latestPlayer;
+                winner = actualPlayer;
                 if (moveNumber == 0) {
                     moveNumber = i + 1;
                 }
             }
         }
-        System.out.println(winner + " wins " + moveNumber);
+        Terminal.printLine(winner + " wins " + moveNumber);
     }
 
-    public String playMove(int m) {
+    /**
+     * This method tests which player is to move and sets his symbol
+     * in the right place on the playing field.
+     * It also returns the actual player.
+     *
+     * @param m the move number
+     * @return the actual player
+     */
+    private String playMove(int m) {
         if (((m + 1) % 2) != 0) {
             gameBoard[this.gameplay[m]] = "X";
             return "P1";
@@ -40,10 +59,20 @@ public final class Main {
         return "undefined";
     }
 
+    /**
+     * Testing if one player has won
+     *
+     * @return true if one player won a row, column or diagonal
+     */
     private boolean checkWin() {
         return (checkColumnsForWin() || checkDiagonalForWin() || checkRowsForWin());
     }
 
+    /**
+     * Testing all 3 columns for a win
+     *
+     * @return true if one player wins a row
+     */
     private boolean checkRowsForWin() {
         for (int i = 0; i < 3; i++)
             if (checkRowsAndColumns(gameBoard[i], gameBoard[i + 1], gameBoard[i + 2])) {
@@ -52,6 +81,11 @@ public final class Main {
         return false;
     }
 
+    /**
+     * Testing all 3 columns for a win
+     *
+     * @return true if one player wins a column
+     */
     private boolean checkColumnsForWin() {
         for (int i = 0; i < 3; i++)
             if (checkRowsAndColumns(gameBoard[i], gameBoard[i + 3], gameBoard[i + 6])) {
@@ -60,16 +94,34 @@ public final class Main {
         return false;
     }
 
+    /**
+     * Testing the diagonals for a win
+     *
+     * @return true if one player wins one of the diagonals
+     */
     private boolean checkDiagonalForWin() {
         boolean diaLeft = checkRowsAndColumns(gameBoard[0], gameBoard[4], gameBoard[8]);
         boolean diaRight = checkRowsAndColumns(gameBoard[2], gameBoard[4], gameBoard[6]);
         return diaLeft || diaRight;
     }
 
+    /**
+     * Testing whether all 3 symbols (X or O) in a row and column are equal
+     *
+     * @param f1 first field in a row or column
+     * @param f2 second field in a row or column
+     * @param f3 third field in a row or column
+     * @return true if all three symbols are equal, false if not
+     */
     private boolean checkRowsAndColumns(String f1, String f2, String f3) {
-        return (((f1 != "-") || (f2 != "-") || (f3 != "-")) && (f1 == f2) && (f2 == f3));
+        return (((!f1.equals("-")) || (!f2.equals("-")) || (!f3.equals("-"))) && (f1.equals(f2)) && (f2.equals(f3)));
     }
 
+    /**
+     * Main method creates new object
+     *
+     * @param args the movement numbers
+     */
     public static void main(String[] args) {
         Main newGame = new Main(args);
         newGame.game();
